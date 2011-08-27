f4aae4a382b114259ceeca73ad701f53ccaeea21
NoahY <noahy57@gmail.com>
2011-08-27 08:16:40 +0530

    fixed conflict with embed

--- qa-smilies-layer.php
+++ qa-smilies-layer.php
@@ -111,26 +111,26 @@
 		function q_view_content($q_view)
 		{
 			if (qa_opt('embed_smileys') && isset($q_view['content'])){
-				$q_view['content'] = $this->embed_replace($q_view['content']);
+				$q_view['content'] = $this->smiley_replace($q_view['content']);
 			}
 			qa_html_theme_base::q_view_content($q_view);
 		}
 		function a_item_content($a_item)
 		{
 			if (qa_opt('embed_smileys') && isset($a_item['content'])) {
-				$a_item['content'] = $this->embed_replace($a_item['content']);
+				$a_item['content'] = $this->smiley_replace($a_item['content']);
 			}
 			qa_html_theme_base::a_item_content($a_item);
 		}
 		function c_item_content($c_item)
 		{
 			if (qa_opt('embed_smileys') && isset($c_item['content'])) {
-				$c_item['content'] = $this->embed_replace($c_item['content']);
+				$c_item['content'] = $this->smiley_replace($c_item['content']);
 			}
 			qa_html_theme_base::c_item_content($c_item);
 		}
 
-		function embed_replace($text) {
+		function smiley_replace($text) {
 			
 
 			foreach($this->smilies as $t => $r) {
